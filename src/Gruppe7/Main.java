6d89f0305f0f026d2a7f27058d1b7080b664d741
Beaxx <bleaxxy@gmail.com>
2018-01-26 22:52:29 +0100

    Genre Check gefixt
    (cherry picked from commit cedbb72)

--- src/Gruppe7/Main.java
+++ src/Gruppe7/Main.java
@@ -49,7 +49,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         /* SETTINGS */
-        int plaeneZuErstellen = 1000; // Max Geschwindigkeit: 3700 pro Sekunde (ohne Optimierung)
+        int plaeneZuErstellen = 100000; // Max Geschwindigkeit: 3700 pro Sekunde (ohne Optimierung)
         OptimierungSwitch = false;
         int mindestPreisVorstellung = 16;
         int maximalPreisVorstellung = 19;
