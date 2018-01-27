f8423887686b248de3471b4ef7c11a39d04fba1a
Beaxx <bleaxxy@gmail.com>
2018-01-27 11:17:25 +0100

    Main: Doku und Formatierung

--- src/Gruppe7/Main.java
+++ src/Gruppe7/Main.java
@@ -12,34 +12,41 @@ import Gruppe7.Importer.*;
  * SETTINGS:
  * /plaeneZuErstellen/
  * Wie viele gültige Spielpläne sollen erstellt werden? Diese Zahl bestimmt über die Dauer des Programmablaufs.
- * Wird die Optimierung der Pläne (OptimierungSwitch = false) ausgeschaltet sind Geschwindigkeiten von
- * 2000 Plänen / Sekunde möglich.
+ * Die Zahl wirkt sich maßgeblich auf dei Dauer des Programmablaufs aus. Wird die Optimierung der Pläne
+ * ausgeschaltet (OptimierungSwitch = false) sind Geschwindigkeiten von über 4000 Plänen / Sekunde möglich.
+ *
+ * /OptimierungsSwitch/
+ * Der Konstruktor der Planer-Klassen enthält einen If-Wrapper, der über den Zustand der OptimierungsSwitch
+ * Variable geschaltet wird. Diese sorgt dafür, dass der Optimierungsalgorithmus für alle Vorstellungen den
+ * optimalen Preis im Intervall von [mindestPreisVorstellung, maximalPreisVortellung] ermittelt. Hierdurch
+ * ergeben sich bessseren Pläne, jedoch starke Performanceeinbußen.
  *
  * /mindestPreisVorstellung/
  * Der niedrigste Preis, den eine Vorstellung haben darf. Wird für die Optimierung verwendet. Ist die
- * Optimierung abgeschaltet -> Kein effekt
+ * Optimierung abgeschaltet hat diese Variable keinen Effekt.
  *
  * /maximalPreisVorstellung/
  * Der höchste Preis, den eine Vorstellung haben darf. Wird für die Optimierung verwendet. Ist die
- * Optimierung abgeschaltet -> Keinen Effekt. Sollte nicht größer als 26 gesetzt werden, da der Andrang
- * für einen Kinofilm bei >26€ Eintrittspreis negativ wird.
+ * Optimierung abgeschaltet hat diese Variable keinen Effekt. Sollte nicht größer als 26 gesetzt werden, da der Andrang
+ * für eine Vorstellung bei Eintrittspreis >26€ null/negativ wird.
  *
- * WICHTIG: Die Differenz zwischen Mindest- und Max-Eintrittspreis entscheided über Geschwindigkeit des programs
- * da jede Wert von MAx -> Min ausprobiert werden muss. Hier ein paar Beispielwerte über die Performance:
+ * WICHTIG: Die Differenz zwischen Mindest- und Max-Eintrittspreis entscheidet über Geschwindigkeit des programs
+ * da jede Wert von MAx -> Min ausprobiert werden muss. Hier ein paar Beispielwerte für die Performance:
  *
- * Zufällige Erstellung ohne Optimierung: 2000 Pläne / Sekunde
+ * Zufällige Erstellung ohne Optimierung mit festem Eintrittspreis: 2000 Pläne / Sekunde
  * Delta = 0: 9 Pläne / Sekunde (z. B. Min: 17, Max: 17) outdated
  * Delta = 1: 4 Pläne / Sekunde (z. B. Min: 17, Max: 18) outdated
  * Delta = 2: 3.2 Pläne / Sekunde (z. B. Min: 16, Max: 18) outdated
  * Delta = 3: 2.2 Pläne / Sekunde (z. B. Min: 16, Max: 19) outdated
  *
  * /mindestBeliebtheit/
- * Setzt den Schwellenwert für die Beliebtheit eines Films. Filme mit niedrigerer Beliebtheit werden nciht importiert.
- * Hat keinen Einfluss auf die Performanz, aber auf das Endergebnis
+ * Setzt den Schwellenwert für die Beliebtheit eines Films. Filme mit niedrigerer Beliebtheit werden nicht importiert.
+ * Hat keinen Einfluss auf die Performance, aber auf das Endergebnis. Je niedriger die Mindestbeliebtheit ist, desto
+ * größer ist die Auswahl an Filmen für einen Spielplan. Es sind dementsprechend mehr Optimierungsläufe durchzufüren,
+ * bis ein Plan in der Region der wirtschaftlich optimalen Pläne gefunden wurde.
  *
  * @author Lennart Völler, Nicole Diestler, Fabian Ueberle
  */
-
 
 public class Main {
 
@@ -48,22 +55,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         /* SETTINGS */
-        int plaeneZuErstellen = 10000; // Max Geschwindigkeit: 3700 pro Sekunde (ohne Optimierung)
+        int plaeneZuErstellen = 10000;
         OptimierungSwitch = false;
         int mindestPreisVorstellung = 12;
         int maximalPreisVorstellung = 14;
         int mindestBeliebtheit = 96;
-        /* SETTINGS */
+        /* /SETTINGS */
 
-        //Datenimport
+        // Datenimport
         new WerbefilmImporter("C:/import/werbespots.csv");
         new SaalImporter("C:/import/saele.csv");
         new KinofilmImporter("C:/import/filme.csv", mindestBeliebtheit);
 
-        //FilmArrays erstellen
+        // FilmArrays erstellen
         FilmVerwaltung.FilmArraysHelper();
 
-        //Werbeplan sortieren und Standard 20 Minunten Block festlegen
+        // Werbeplan sortieren und Standard 20 Minunten Block festlegen
         WerbefilmVerwaltung.werbeplanSortieren();
         WerbefilmVerwaltung.standardWerbeblock();
 
@@ -71,35 +78,35 @@ public class Main {
         SaalVerwaltung.saalplanSortieren();
         SaalVerwaltung.plaetzteInGroestemUndZweitgroestemSaal();
 
-        //Performance Wrapper start
+        // Performance Wrapper start
         long startTime = System.currentTimeMillis();
 
-        //Algorithmus
-        Planer planer = new Planer(mindestPreisVorstellung, maximalPreisVorstellung);
-        for (int i = 0; i < plaeneZuErstellen; i++)
+            // Algorithmus
+            Planer planer = new Planer(mindestPreisVorstellung, maximalPreisVorstellung);
+            for (int i = 0; i < plaeneZuErstellen; i++)
 
-        {
-            Planer tempPlaner = new Planer(mindestPreisVorstellung, maximalPreisVorstellung);
+            {
+                Planer tempPlaner = new Planer(mindestPreisVorstellung, maximalPreisVorstellung);
 
-            if (tempPlaner.getSpielplanGewinn() > planer.getSpielplanGewinn()) {
-                planer = tempPlaner;
-                System.out.println("Ticket Einnahmen: " + planer.getSpielplanEinnahmenAusKartenverkaeufen() + "\n" +
-                        "Werbungs Einnahmen: " + planer.getSpielplanWerbungsEinnahmen() + "\n" +
-                        "Ausgaben: " + planer.getSpielplanAusgaben() + "\n" +
-                        "Gewinn:" + planer.getSpielplanGewinn() + "\n" +
-                        "--------------------------------");
+                if (tempPlaner.getSpielplanGewinn() > planer.getSpielplanGewinn()) {
+                    planer = tempPlaner;
+                    System.out.println("Ticket Einnahmen: " + planer.getSpielplanEinnahmenAusKartenverkaeufen() + "\n" +
+                            "Werbungs Einnahmen: " + planer.getSpielplanWerbungsEinnahmen() + "\n" +
+                            "Ausgaben: " + planer.getSpielplanAusgaben() + "\n" +
+                            "Gewinn:" + planer.getSpielplanGewinn() + "\n" +
+                            "--------------------------------");
+                }
             }
-        }
 
-        //Performance Wrapper ende
+        // Performance Wrapper ende
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
         long totalTimeS = totalTime / 1000;
 
-        // Ausgabe
-        System.out.println(Arrays.deepToString(planer.getSpielplan())); //Bester Spielplan
+        // Ausgabe bester Spielplan
+        System.out.println(Arrays.deepToString(planer.getSpielplan()));
 
-        //Performance Auswertung
+        // Ausgabe Laufdauer und Geschwindigkeit
         System.out.println(totalTimeS + " Sekunden für " + plaeneZuErstellen + " Durchläufe" + "\n" +
                 (double) plaeneZuErstellen / totalTimeS + " pro Sekunde");
     }
