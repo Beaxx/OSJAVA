f8059ca8c8a94ef102310cc4837c20bec3bbc40a
Beaxx <bleaxxy@gmail.com>
2018-01-27 11:54:06 +0100

    Spielzeiten Doku: FABIAN

--- src/Gruppe7/Data/Spielzeiten.java
+++ src/Gruppe7/Data/Spielzeiten.java
@@ -1,5 +1,8 @@
 package Gruppe7.Data;
 
+/**
+ * @author Fabian Ueberle
+ */
 public enum Spielzeiten {
 
     SLOT_1500(150),
@@ -8,22 +11,43 @@ public enum Spielzeiten {
     SLOT_2300(150);
 
     private int slotDuration;
-    Spielzeiten(int slotDuration){
+
+    /**
+     * Was macht diese Methode?
+     *
+     * @param slotDuration Die Spieldauer eines Timeslots in Minuten
+     */
+    Spielzeiten(int slotDuration) {
         this.slotDuration = slotDuration;
     }
 
-    public int getSlotDuration(){
+    /**
+     * Getter für die Spieldauer eines Timeslots
+     *
+     * @return Die Spielzeit in Minuten
+     */
+    public int getSlotDuration() {
         return this.slotDuration;
     }
 
+    /**
+     * ToString Override, um die Uhrzeit eines Timeslots lesbar darzustellen.
+     *
+     * @return Die Uhrzeit eines Timeslots im Format "XX:XX Uhr"
+     */
     @Override
     public String toString() {
-        switch(this){
-            case SLOT_1500: return "15:00 Uhr";
-            case SLOT_1730: return "17:30 Uhr";
-            case SLOT_2000: return "20:00 Uhr";
-            case SLOT_2300: return "23:00 Uhr";
-            default: return "--:-- Uhr";
+        switch (this) {
+            case SLOT_1500:
+                return "15:00 Uhr";
+            case SLOT_1730:
+                return "17:30 Uhr";
+            case SLOT_2000:
+                return "20:00 Uhr";
+            case SLOT_2300:
+                return "23:00 Uhr";
+            default:
+                return "--:-- Uhr";
         }
     }
 }
